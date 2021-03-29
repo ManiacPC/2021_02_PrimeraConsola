b774c2a0b40e9c8dfe1c0a24730b15c12e82f626
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-03-29 15:02:40 -0300

    Agregar archivos de proyecto.

---
+++ PrimeraConsola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mensajes al usuario
+            Console.WriteLine("Bienvenido a la aplicación de consola!");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Por favor elija una opción:\n");
+            Console.WriteLine("a) Una opción prioritaria");
+            Console.WriteLine("b) Una opción secundaria");
+            Console.WriteLine("c) No existe");
+
+            string opcion = Console.ReadLine(); // Lectura desde el teclado a variable opcion
+
+            // Revisar las opciones y sus procesos
+            switch (opcion) // opcion = c
+            {
+                case "a":
+                    // ejecutar alguna línea de código
+                    Console.WriteLine("Eligió la opción a");
+                    break;
+                case "b":
+                    Console.WriteLine("Eligió la opción b");
+                    break;
+                default:
+                    Console.WriteLine("No existe la opción seleccionada.");
+                    break;
+
+            }
+
+            Console.ReadKey(); // Pedir al usuario que ingrese un caracter (pero no se guarda)
+        }
+    }
+}
