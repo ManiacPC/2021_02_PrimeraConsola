d41c8615316ce99f7dd1f186a72ee78198f24e22
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-03-29 15:15:11 -0300

    Cambio de aplicación de consola simple a una iterada hasta una salida voluntaria del usuario

--- PrimeraConsola/Program.cs
+++ PrimeraConsola/Program.cs
@@ -10,35 +10,43 @@ namespace PrimeraConsola
     {
         static void Main(string[] args)
         {
-            // Interfaz de consola sin que se repitan las opciones voluntariamente
+            // Interfaz de consola con opciones repetidas hasta interrupción voluntaria
 
-            // Mensajes al usuario
             Console.WriteLine("Bienvenido a la aplicación de consola!");
             Console.WriteLine("--------------------------------------");
-            Console.WriteLine("Por favor elija una opción:\n");
-            Console.WriteLine("a) Una opción prioritaria");
-            Console.WriteLine("b) Una opción secundaria");
-            Console.WriteLine("c) No existe");
 
-            string opcion = Console.ReadLine(); // Lectura desde el teclado a variable opcion
-
-            // Revisar las opciones y sus procesos
-            switch (opcion) // opcion = c
+            while (true)
             {
-                case "a":
-                    // ejecutar alguna línea de código
-                    Console.WriteLine("Eligió la opción a");
-                    break;
-                case "b":
-                    Console.WriteLine("Eligió la opción b");
-                    break;
-                default:
-                    Console.WriteLine("No existe la opción seleccionada.");
-                    break;
+                // Mensajes al usuario
+                Console.WriteLine("Por favor elija una opción:\n");
+                Console.WriteLine("a) Una opción prioritaria");
+                Console.WriteLine("b) Una opción secundaria");
+                Console.WriteLine("x) salir");
 
-            }
+                string opcion = Console.ReadLine(); // Lectura desde el teclado a variable opcion
 
-            Console.ReadKey(); // Pedir al usuario que ingrese un caracter (pero no se guarda)
+                // Revisar las opciones y sus procesos
+                switch (opcion) // opcion = c
+                {
+                    case "a":
+                        // ejecutar alguna línea de código
+                        Console.WriteLine("Eligió la opción a");
+                        break;
+                    case "b":
+                        Console.WriteLine("Eligió la opción b");
+                        break;
+                    case "x":
+                        Environment.Exit(0); // 0 = SIN ERRORES, SALIDA NORMAL
+                        break;
+                    default:
+                        Console.WriteLine("No existe la opción seleccionada.");
+                        break;
+
+                }
+
+                Console.ReadKey(); // Pedir al usuario que ingrese un caracter (pero no se guarda)
+                Console.Clear();
+            }            
         }
     }
 }
